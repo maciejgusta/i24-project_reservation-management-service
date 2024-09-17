ce14b18fdfcf79f969e63261ad0801d3fee9b4e1
Maciej Gusta <maciej.gusta@gmail.com>
2024-09-17 17:37:19 +0200

    login html

--- html/index.php
+++ html/index.php
@@ -7,35 +7,14 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<p>html working</p>
-    <?php
-        $server_name="localhost";
-        $username="admin";
-        $password="admin";
-        $dbname="jadzia";
-        $conn = new mysqli($server_name, $username, $password, $dbname);
-	
-	if ($conn->connect_error) {
-            die("connection failed: ".$conn->connect_error);
-        }
-        echo "<p>Connection to db: OK</p>";
-                
-	/*
-	echo "<table>\n";
-        $idx = 1;	
-        while ($idx <= $table->num_rows){
-            echo "<tr>\n";
-            $row = $table->fetch_assoc();
-            //print_r($row);
-            foreach ($row as $x => $y){
-                echo "<td>$y</td>\n";
-            }
-            $idx++;   
-            echo "</tr>\n";
-        }
-        echo "</table>\n";
-	 */
-?>
-
+	<div id="loginc">
+        <form action="login.php" method="post">
+            <label for="username">Login:</label>
+            <input type="text" id="username" name="username" required>
+            <label for="password">Password:</label>
+            <input type="text" id="password" name="password" required>
+            <button type="submit" name="login">Login</button>
+        </form>
+    </div>
 </body>
 </html>
