4b35b74c0745f5a8ca2bcdce057849d8c6785087
Maciej Gusta <maciej.gusta@gmail.com>
2024-09-18 15:28:59 +0200

    auto push

---
+++ html/home.php
@@ -0,0 +1,19 @@
+<?php   
+    session_star();
+    $username = isset($_SESSION['username']) ? $_SESSION['username'] : "none";
+    unset($_SESSION['username']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <?php
+    echo "$username";
+    ?>
+</body>
+</html>
--- html/index.php
+++ html/index.php
@@ -11,7 +11,7 @@ unset($_SESSION['error']); // Clear the session error after reading it
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
