df853dc984170e210cac2a57c1aa32cb893a8a83
Maciej Gusta <maciej.gusta@gmail.com>
2024-09-18 18:59:54 +0200

    auto push

--- html/index.php
+++ html/index.php
@@ -13,29 +13,23 @@ unset($_SESSION['error']); // Clear the session error after reading it
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet"> 
 </head>
 <body>
 	<div id="login_container">
-        <h2>Login</h2>
         <form id="login_form" action="login.php" method="post">
             <label for="username">Username:</label>
-            <input id="login_input1" type="text" name="username" placeholder="Enter your username" required>
-            
+            <input id="login_input1" type="text" name="username" required>
             <label for="password">Password:</label>
-            <input id="login_input2" type="password" name="password" placeholder="Enter your password" required>
-            
+            <input id="login_input2" type="password" name="password" required><br>
             <?php
                 if ($error == true){
                     echo "<p id=\"invalid_credentials\">Invalid username or password!</p>";
                 }
             ?>
-            <button type="submit" name="login" class="primary_button">Log in</button>
+            <button type="submit" name="login">Log in</button>
         </form>
-        
-        <p>Don't have an account?</p>
-        <form id="sign_up_form" action="sign_up.php" method="post">
-            <button type="submit" name="sign_up" class="secondary_button">Sign up</button>
+        <form id="sign_up_form" style="margin-top: 10px" action="sign_up.php" method="post">
+            <button type="submit" name="sign_up">Sign up</button>
         </form>
     </div>
 </body>
