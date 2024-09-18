ad234768f73d872af364bd76fef929a365d62066
Maciej Gusta <maciej.gusta@gmail.com>
2024-09-18 15:18:33 +0200

    auto push

--- html/sign_up_process.php
+++ html/sign_up_process.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-// Enable error reporting for debugging
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+//Enable error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 if (isset($_POST['sign_up'])){
