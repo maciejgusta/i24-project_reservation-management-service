5dea5caf8f7b5a74bbef1287682ec15d80cd9cf1
Ubuntu <ubuntu@ip-172-31-9-143.eu-central-1.compute.internal>
2024-09-18 08:24:01 +0000

    sign up backend in progress

--- html/sign_up.php
+++ html/sign_up.php
@@ -1,6 +1,7 @@
 <?php
-    session_start();
-    $username = isset($_SESSION["username"]) ? $_SESSION["username"] : "";
+session_start();
+$error = isset($_SESSION['error']) ? $_SESSION['error'] : false; // Default to false if not set
+unset($_SESSION['error']); // Clear the session error after reading it
 ?>
 
 <!DOCTYPE html>
@@ -12,25 +13,25 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div id="sign_up_container" action="sign_up.php" method="post">
-        <form id="sign_up_form">
+    <div id="sign_up_container">
+        <form id="sign_up_form" action="sign_up_process.php" method="post">
             <label for="username">Username:</label>
-            <input type="text" name="username">
+            <input type="text" name="username" required>
             <label for="password">Password:</label>
-            <input type="password" name="password">
+            <input type="password" name="password" required>
             <label for="repeat_password">Repeat password:</label>
-            <input type="password" name="repeat_password">
+            <input type="password" name="repeat_password" required>
             <label for="first_name">First name:</label>
-            <input type="text" name="first_name">
+            <input type="text" name="first_name" required>
             <label for="last_name">Last name:</label>
-            <input type="text" name="lastname">
+            <input type="text" name="last_name" required>
             <label for="phone_number">Phone number:</label>
-            <input type="text" name="phone_number">
-            <button type="submit">Sign up</button>
+            <input type="number" name="phone_number" required>
+            <button type="submit" name="sign_up">Sign up</button>
+            <?php
+                echo "<p id=\"invalid_credentials\">$error</p>";
+            ?>
         </form>
-        <?php
-            echo "$username";
-        ?>
     </div>
 </body>
 </html>
