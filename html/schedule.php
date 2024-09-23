1d893659cb92d17cf1b4c6952a11175f70f11722
Ubuntu <ubuntu@ip-172-31-9-143.eu-central-1.compute.internal>
2024-09-23 16:07:15 +0000

    schedule.php in progress, time to add backend for this

--- html/schedule.php
+++ html/schedule.php
@@ -9,19 +9,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Schedule</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/schedule.css">
 </head>
 <body>
     <?php
         $date = new DateTime();
         $day = (int)$date->format("w");
-        $chg = "-".(6-$day)." days";
-        //echo "$date   $chg";
+        $chg = $day == 0 ? "+1 days" : "-".($day-1)." days";
         $date->modify($chg);
 
         $ws = $date->format("d-m-Y");
-        $we = $date->modify("+6 days")->format("d-m-Y");
-        echo "<h1 style=\"text-align: center\">".$ws." - ".$we."</h1>";
+        $we = $date->modify("+5 days")->format("d-m-Y");
+
+        $date->modify("-5 days");
 
         $servername = "localhost";
         $dbusername = "admin";
@@ -35,35 +35,60 @@
         //         print_r($visit);
         //     }
         // }
-        echo "<div class=\"calendar_block\">
-        <div class=\"dotted_lines\" id=\"dl1\"></div>
-        <div class=\"dotted_lines\" id=\"dl2\"></div>
-        <div class=\"dotted_lines\" id=\"dl3\"></div>
-        <div class=\"dotted_lines\" id=\"dl4\"></div>
-        <div class=\"dotted_lines\" id=\"dl5\"></div>
-        <div class=\"dotted_lines\" id=\"dl6\"></div>
-        <div class=\"dotted_lines\" id=\"dl7\"></div>
-        <div class=\"dotted_lines\" id=\"dl8\"></div>
-        <div class=\"dotted_lines\" id=\"dl9\"></div>
-        <div class=\"time_cell\">
-            <div class=\"hour_cell\">9:00</div>
-            <div class=\"hour_cell\">10:00</div>
-            <div class=\"hour_cell\">11:00</div>
-            <div class=\"hour_cell\">12:00</div>
-            <div class=\"hour_cell\">13:00</div>
-            <div class=\"hour_cell\">14:00</div>
-            <div class=\"hour_cell\">15:00</div>
-            <div class=\"hour_cell\">16:00</div>
-            <div class=\"hour_cell\">17:00</div>
-            <div class=\"hour_cell\">18:00</div>
+        echo '
+        <div class="calendar_block">
+
+        <div class="date_block">
+
+            <div class="logo_cell"></div>
+        ';
+
+        for ($i = 0; $i < 6; $i++){
+            $cur_date = $date->format("d-m-Y");
+            $cur_wk = $date->format("D");
+            echo '<div class="date_cell">'.$cur_date.' ('.$cur_wk.')</div>';
+            $date->modify("+1 days");
+        }
+
+        echo '
         </div>
-        <div class=\"day_cell\"></div>
-        <div class=\"day_cell\"><div class=\"event_cell\">test</div></div>
-        <div class=\"day_cell\"></div>
-        <div class=\"day_cell\"></div>
-        <div class=\"day_cell\"></div>
-        <div class=\"day_cell\"></div>
-        </div>";
+
+        <div class="time_block">
+
+            <div class="hour_block">
+                <div class="hour_cell">8:00</div>
+                <div class="hour_cell">9:00</div>
+                <div class="hour_cell">10:00</div>
+                <div class="hour_cell">11:00</div>
+                <div class="hour_cell">12:00</div>
+                <div class="hour_cell">13:00</div>
+                <div class="hour_cell">14:00</div>
+                <div class="hour_cell">15:00</div>
+                <div class="hour_cell">16:00</div>
+                <div class="hour_cell">17:00</div>
+            </div>
+
+            <div class="day_block"></div>
+            <div class="day_block"></div>
+            <div class="day_block"></div>
+            <div class="day_block"></div>
+            <div class="day_block"></div>
+            <div class="day_block"></div>
+
+            <div class="line" id="l10"></div>
+            <div class="line" id="l20"></div>
+            <div class="line" id="l30"></div>
+            <div class="line" id="l40"></div>
+            <div class="line" id="l50"></div>
+            <div class="line" id="l60"></div>
+            <div class="line" id="l70"></div>
+            <div class="line" id="l80"></div>
+            <div class="line" id="l90"></div>
+
+        </div>
+    
+        </div>
+        ';
 
     ?>
 </body>
--- html/sign_up.php
+++ html/sign_up.php
@@ -10,7 +10,7 @@ unset($_SESSION['error']); // Clear the session error after reading it
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>\Sign up</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body id="body_form">
     <div id="sign_up_container">
