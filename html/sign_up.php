ac3ea6d993bd92d17f1e37a0eddd81689bc1b20a
Ubuntu <ubuntu@ip-172-31-9-143.eu-central-1.compute.internal>
2024-09-24 21:08:50 +0000

    auto push

--- html/sign_up.php
+++ html/sign_up.php
@@ -10,27 +10,33 @@ unset($_SESSION['error']); // Clear the session error after reading it
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>\Sign up</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/sign_up.css">
 </head>
 <body id="body_form">
-    <div id="sign_up_container">
+    <div id="sign_up_block">
         <form id="sign_up_form" action="sign_up_process.php" method="post">
-            <label for="username">Username:</label>
-            <input class="sign_up_input" type="text" name="username" required>
-            <label for="password">Password:</label>
-            <input class="sign_up_input" type="password" name="password" required>
-            <label for="repeat_password">Repeat password:</label>
-            <input class="sign_up_input" type="password" name="repeat_password" required>
-            <label for="first_name">First name:</label>
-            <input class="sign_up_input" type="text" name="first_name" required>
-            <label for="last_name">Last name:</label>
-            <input class="sign_up_input" type="text" name="last_name" required>
-            <label for="phone_number">Phone number:</label>
-            <input class="sign_up_input" type="number" name="phone_number" required>
-            <button id="sign_up_button" type="submit" name="sign_up">Sign up</button>
+            <label class="label" for="username">Username:</label>
+            <input class="input" type="text" name="username" required>
+            <label class="label" for="password">Password:</label>
+            <input class="input" type="password" name="password" required>
+            <label class="label" for="repeat_password">Repeat password:</label>
+            <input class="input" type="password" name="repeat_password" required>
+            <label class="label" for="first_name">First name:</label>
+            <input class="input" type="text" name="first_name" required>
+            <label class="label" for="last_name">Last name:</label>
+            <input class="input" type="text" name="last_name" required>
+            <label class="label" for="phone_number">Phone number:</label>
+            <input class="input" type="number" name="phone_number" required>
+
+            <div id="credentials_block">
             <?php
-                echo "<p id=\"invalid_credentials\">$error</p>";
+                if ($error){
+                    echo "<div id=\"credentials_div\">$error</div>";
+                }  
             ?>
+            </div>
+            <button id="sign_up_button" type="submit" name="sign_up">Sign up</button>
+            
         </form>
     </div>
 </body>
